279d29929b5a3bca5673ba0a9bb97b3fc510451a
scott <scott.o'neill@serco-ap.com>
2020-07-24 11:05:32 +1000

    Added Staticfiles and configured CSS

--- FeedbackForm/urls.py
+++ FeedbackForm/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('forms.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
